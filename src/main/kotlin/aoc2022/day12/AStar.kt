11d96e41fb2a9f1a8a52f0aa9020685e540964ed
Jochum Börger <jborger@qxperts.io>
2022-12-12 12:51:36 +0100

    Day 12 - move all mutable state inside function scope

--- src/main/kotlin/aoc2022/day12/AStar.kt
+++ src/main/kotlin/aoc2022/day12/AStar.kt
@@ -10,11 +10,20 @@ data class Node<T>(
 
 data class Edge<T>(val to: T, val cost: Int = 1)
 
-class AStar<T>(private val nodes: Map<T, Node<T>>) : Comparator<Node<T>> {
+class AStar<T>(private val nodes: Map<T, Node<T>>) {
 
     fun shortestPath(start: Node<T>, end: Node<T>): List<Node<T>> {
-        val processed: PriorityQueue<Node<T>> = PriorityQueue(this)
-        val unprocessed: PriorityQueue<Node<T>> = PriorityQueue(this)
+        val costs: MutableMap<T, Int> = mutableMapOf()
+        val moves: MutableMap<T, Int> = mutableMapOf()
+        val parents: MutableMap<T, Node<T>> = mutableMapOf()
+
+        val comparator: Comparator<Node<T>> = Comparator { node1, node2 ->
+            costs.getOrDefault(node1.id, Int.MAX_VALUE) - costs.getOrDefault(node2.id, Int.MAX_VALUE)
+        }
+
+        val processed: PriorityQueue<Node<T>> = PriorityQueue(comparator)
+        val unprocessed: PriorityQueue<Node<T>> = PriorityQueue(comparator)
+
         moves[start.id] = 0
         costs[start.id] = start.heuristicFunction(end)
         unprocessed.add(start)
@@ -27,14 +36,14 @@ class AStar<T>(private val nodes: Map<T, Node<T>>) : Comparator<Node<T>> {
 
             for (edge in node.neighbours) {
                 val next = nodes[edge.to]!!
-                val nextMove = moveFor(node) + edge.cost
+                val nextMove = moves.getOrDefault(node.id, Int.MAX_VALUE) + edge.cost
                 if (!unprocessed.contains(next) && !processed.contains(next)) {
                     parents[next.id] = node
                     moves[next.id] = nextMove
                     costs[next.id] = nextMove + next.heuristicFunction(end)
                     unprocessed.add(next)
                 } else {
-                    if (nextMove < moveFor(next)) {
+                    if (nextMove < moves.getOrDefault(next.id, Int.MAX_VALUE)) {
                         parents[next.id] = node
                         moves[next.id] = nextMove
                         costs[next.id] = nextMove + next.heuristicFunction(end)
@@ -45,33 +54,13 @@ class AStar<T>(private val nodes: Map<T, Node<T>>) : Comparator<Node<T>> {
                     }
                 }
             }
+
             unprocessed.remove(node)
             processed.add(node)
         }
 
-        return backTrace(end)
+        return generateSequence(end) { parents[it.id] }.toList().reversed()
     }
 
-    private fun backTrace(end: Node<T>): List<Node<T>> {
-        var node: Node<T> = end
-        val path: MutableList<Node<T>> = mutableListOf()
-
-        while (parents[node.id] != null) {
-            path.add(node)
-            node = parents[node.id]!!
-        }
-        path.add(node)
-
-        return path.toList().reversed()
-    }
-
-    private val costs: MutableMap<T, Int> = mutableMapOf()
-    private val moves: MutableMap<T, Int> = mutableMapOf()
-    private val parents: MutableMap<T, Node<T>> = mutableMapOf()
-
-    private fun costFor(node: Node<T>) = costs.getOrDefault(node.id, Int.MAX_VALUE)
-    private fun moveFor(node: Node<T>) = moves.getOrDefault(node.id, Int.MAX_VALUE)
-
-    override fun compare(node1: Node<T>, node2: Node<T>): Int = costFor(node1) - costFor(node2)
 }
 
